38f1be6e84d1bfb65891f1e65adc2c8cc2646870
Nataliia Dobrowolska <nmdobrowolska@gmail.com>
2020-04-27 09:01:42 +0200

    Date, Regex, and Promise tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  return (year % 100 === 0) ? (year % 400 === 0) : (year % 4 === 0);
 }
 
 
@@ -73,8 +74,17 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const timespan = endDate.getTime() - startDate.getTime();
+  const hours = Math.floor((timespan / (1000 * 60 * 60)) % 100);
+  const minutes = Math.floor((timespan / (1000 * 60)) % 60);
+  const seconds = Math.floor((timespan / 1000) % 60);
+  const milisecondsStr = (timespan % 1000).toString().length === 1 ? '000' : (timespan % 1000).toString();
+  const hoursStr = hours.toString().length < 2 ? `0${hours.toString()}` : hours.toString();
+  const minutesStr = minutes.toString().length < 2 ? `0${minutes.toString()}` : minutes.toString();
+  const secondsStr = seconds.toString().length < 2 ? `0${seconds.toString()}` : seconds.toString();
+
+  return `${hoursStr}:${minutesStr}:${secondsStr}.${milisecondsStr}`;
 }
 
 
--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,8 +28,16 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise(((resolve, reject) => {
+    if (isPositiveAnswer === undefined) {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    } else if (!isPositiveAnswer) {
+      resolve('Oh no, she said "No".');
+    } else {
+      resolve('Hooray!!! She said "Yes"!');
+    }
+  }));
 }
 
 
@@ -48,8 +56,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +79,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,8 +100,12 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  const result = [];
+  const prom = Promise.resolve(array);
+  return prom.then((promises) => {
+    promises.map((promise) => promise.then((value) => result.push(value)).catch(() => 0));
+  }).then(() => result.reduce(action));
 }
 
 module.exports = {
